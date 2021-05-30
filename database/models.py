bf52c5eeb46aad3edaab36b3e0060d061bf204b3
Crazydevil1996 <ps12shanty@gmail.com>
2021-05-30 22:18:00 +0530

    App to live on Heroku

--- database/models.py
+++ database/models.py
@@ -1,5 +1,6 @@
+from enum import unique
 from database.db import db
-
+from flask_bcrypt import check_password_hash, generate_password_hash
 
 class books(db.Document):
     b_id = db.IntField(required=True, unique=True)
@@ -11,3 +12,12 @@ class books(db.Document):
     pages = db.IntField(required=True)
     title = db.StringField(required=True)
     year = db.IntField(required=True)
+
+class  User(db.Document):
+    email = db.EmailField(required=True, unique=True)
+    password = db.StringField(required=True, min_length=6)
+    def hash_password(self):
+        self.password = generate_password_hash(self.password).decode('utf8')
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
